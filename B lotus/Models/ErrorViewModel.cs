9e877b22eca85dbf299686f9a4d4002ab221846c
Rojay <rojaychamlagain5143@gmail.com>
2022-01-30 20:25:13 +0545

    Add project files.

---
+++ B lotus/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace B_lotus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
